e533121880abfa846e632b8e13b94928a438200a
earldean <earl.dean@res-group.com>
2020-03-20 16:42:17 -0600

    initial commit

---
+++ TestProj/Class1.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TestProj
+{
+    public class Class1
+    {
+        private SqliteConnection connection;
+        public Class1()
+        {
+            connection = new SqliteConnection(new SqliteConnectionStringBuilder
+            {
+                DataSource = ":memory:"
+            }.ToString());
+        }
+
+        [Fact]
+        public void RunTest()
+        {
+            Assert.True(true);
+        }
+    }
+}
